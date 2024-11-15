f1f354e2ca0ce5c601d2cff3b08831501f7cd2ff
Quateem Gaffney-Smith <90237467+qgaffney@users.noreply.github.com>
2024-11-14 20:23:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,73 @@
-def load_data(title):
-  pass
-
 #Create Book class
 class Book:
-  #Provide attributes
-  def __init__(self, title, author, genre, pages):
-  #Constructor to initialize
-    self.title = title
-    self.author = author
-    self.genre = genre
-    self.pages = pages
-    self.data = load_data(title)
-    self.read = False
+    #Provide attributes
+    def __init__(self, title, author, genre, pages):
+        #Constructor to initialize
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.pages = pages
+        self.read = False
 
-  #Define description() method
-  def description(self):
-    #Return string
-    return f"Title: {self.title}, Author: {self.author}, Genre: {self.genre}, Pages: {self.pages}"
+    #Define description() method
+    def description(self):
+        #Return string
+        return f"Title: {self.title}, Author: {self.author}, Genre: {self.genre}, Pages: {self.pages}"
 
-  #Define marked_as_read() method
-  def marked_as_read(self):
-    #Set read to True
-    self.read = True
-    #Print message
-    print(f"{self.title} by {self.author} has been read.")
+    #Define marked_as_read() method
+    def marked_as_read(self):
+        #Set read to True
+        self.read = True
+        #Print message
+        print(f"{self.title} by {self.author} has been read.")
 
-  def add_book(self, title):
-    self.books.append(title)
+    def filter_books(books, filter_by, search_term):
+        filtered_books = []
+        for book in books:
+            if search_term.lower() in book[filter_by].lower():
+                filtered_books.append(book)
+            return filtered_books 
 
-  def display_books(self):
-    print(f"Books in {self.genre}:")
-    for title in self.titles:
-      print(title)
+    def display_books(books):
+        for book in books:
+            print(f"Title: {book['title']}, Author: {book['author']}, Genre: {book['genre']}, Pages: {book['pages']}")
 
-#Create book object 1
-book1 = Book("Becoming Supernatural: How Common People Are Doing the Uncommon", "Dr. Joe Dispenza", "Self-Help", 384)
-#Print description
-print(book1.description())
-#Mark as read
-book1.marked_as_read()
+    def display_all_books(books_list):
+        print("Please choose an option:")
 
-#Create book object 2
-book2 = Book("The S9even Spiritual Laws of Success: A Practical Guide to the Fulfillment of Your Dreams", "Deepak Chopra", "Self-Help", 118)
-#Print descriptions=
-print(book2.description())
 
-#Create User class
-class User:
-  #Provide attributes
-  def __init__(self, name, accnt):
-    #Constructor to initialize
-    self.name = name
-    self.accnt = accnt
-    self.shelf = []
-    self.balance = 0
+    if __name__ == "__main__":
+        books_list = [
+            {"title": "Becoming Supernatural: How Common People Are Doing the Uncommon", "author": "Dr. Joe Dispenza", "genre": "Self-Help", "pages": 384, marked_as_read: True},
+            {"title": "The Silent Patient", "author": "Alex Michaelides", "genre": "Psychological Thriller", "pages": 336},
+            {"title": "Catch-22", "author": "Joseph Heller", "genre": "Dark Comedy", "pages": 453}
+        ]
+    
+        all_books = "{books_list.title}"
 
-  #Define buy_book
-  def buy_book(self, book):
-    self.book += book1[book] and book2[book]
-    self.shelf += Book(book1 and book2)
+        while True:
+            print("\nChoose filter option:")
+            print("1. Display All Books")
+            print("2. Filter by Genre")
+            print("3. Search by Author")
+            print("4. Exit")
+            choice = input("Enter your choice: ")
 
-  #Define read_book
-  def read_book(self, book):
-    book = self.shelf[self.shelf.find(book)]
-
+            if choice == '1':
+                while True:
+                    if 1:
+                        choice = display_all_books
+                        print(all_books)
+                        break
+            elif choice == '2':
+                search_term = input("Enter genre: ")
+                filtered_books = filter_books(books_list, "genre", search_term)
+                display_books(filtered_books)
+            elif choice == '3':
+                search_term = input("Enter author: ")
+                filtered_books = filter_books(books_list, "author", search_term)
+                display_books(filtered_books)
+            elif choice == '4':
+                break
+            else:
+                print("Invalid choice.")
